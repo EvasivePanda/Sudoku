1f47d6730eaad1bf4fc13463a1295446fb0b7d6c
Tim <61515769+EvasivePanda@users.noreply.github.com>
2021-01-18 12:20:09 -0600

    Some fun randomness

--- DeskTopSudoku/src/SudokuDriver.java
+++ DeskTopSudoku/src/SudokuDriver.java
@@ -1,111 +1,71 @@
 import java.util.Scanner;
 
-
-
-
 public class SudokuDriver {
-	
-	/** This is the scanner used to get the user inputs for the boards size.*/
+
+	/** This is the scanner used to get the user inputs for the boards size. */
 	private Scanner userInput = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//SudokuDriver sd = new SudokuDriver();
-		//sd.testy();
-	//	sd.makeGame();
-		frameClass fc = new frameClass();
-		//fc    fgdssdm n 
-		//fc.makeBoxForGame(null);
+		SudokuDriver sd = new SudokuDriver();
+		sd.testy();
+		// sd.makeGame();
+		// frameClass fc = new frameClass();
+		// fc fgdssdm n
+		// fc.makeBoxForGame(null);
 	}
-	
-	
-	
-	
-	
-	
 
-	public void makeGame(){
+	public void makeGame() {
 		/**
-		JFrame frame = new JFrame("FrameDemo");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//3. Create components and put them in the frame.
-		//...create emptyLabel...
-		frame.setSize(800,600);
-
-		//4. Size the frame.
-		//frame.pack();
-
-		//5. Show it.
-		frame.setVisible(true);
-		frame.setTitle("Sudoku Game");
-	//	JFrame.setDefaultLookAndFeelDecorated(true);
-		//JFrame fdrame = new JFrame("A window");
-*/
-
+		 * JFrame frame = new JFrame("FrameDemo");
+		 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //3. Create components
+		 * and put them in the frame. //...create emptyLabel... frame.setSize(800,600);
+		 * 
+		 * //4. Size the frame. //frame.pack();
+		 * 
+		 * //5. Show it. frame.setVisible(true); frame.setTitle("Sudoku Game"); //
+		 * JFrame.setDefaultLookAndFeelDecorated(true); //JFrame fdrame = new JFrame("A
+		 * window");
+		 */
 
 	}
-	
-	
-	
-	public void testy(){
-		
+
+	public void testy() {
+
 		SudokuChecker sc = new SudokuChecker();
 		Board b = new Board();
 		Board c;
 		SudokuMaker sm = new SudokuMaker(b);
-		//c = sm.boardMakesy();
-		//c=makeValidDoku(b);
-		c=makeHardestSudoku(b);
-		frameClass fc = new frameClass();
-//fc.makeBoxForGame(c.getStringBoard());
-	//	fc.test();
-	//	c.finalizeBoard();
-	SudokuSolver ss= new SudokuSolver();
-		//b = sm.boardMakesy();
-	//    c.printBoard();
-		if(ss.solve(c)){
+		// c = sm.boardMakesy();
+		c = makeValidDoku(b);
+		// c = makeHardestSudoku(b);
+		// frameClass fc = new frameClass();
+		// fc.makeBoxForGame(c.getStringBoard());
+		// fc.test();
+		// c.finalizeBoard();
+		SudokuSolver ss = new SudokuSolver();
+		// b = sm.boardMakesy();
+		c.printBoard();
+		if (ss.solve(c)) {
 			System.out.println(" Things possible");
 			c.printBoard();
-		}else{
-		System.out.println("Sorry not possible");
-	}
-		//window.print(c.getStringBoard());
-		//c.printBoard();
-	//	c.printBoard();
-		//System.out.println("                                        ?"+ss.solveSud(c,0,0,1));
+		} else {
+			System.out.println("Sorry not possible");
+		}
+		// window.print(c.getStringBoard());
+		// c.printBoard();
+		// c.printBoard();
+		// System.out.println(" ?"+ss.solveSud(c,0,0,1));
 
-		//Board b = new Board();
-		//b.printBoard();
-	//	sc.isValidSudoku(b);
-	//	c.placeValue(8, 0, 0);
-	//	b.removeValue(0, 0);
+		// Board b = new Board();
+		// b.printBoard();
+		// sc.isValidSudoku(b);
+		// c.placeValue(8, 0, 0);
+		// b.removeValue(0, 0);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public Board makeHardestSudoku(Board b){
+
+	public Board makeHardestSudoku(Board b) {
 		b.placeValue(8, 0, 0);
 		b.placeValue(3, 1, 2);
 		b.placeValue(6, 1, 3);
@@ -129,9 +89,9 @@ public class SudokuDriver {
 		b.placeValue(4, 8, 6);
 		return b;
 	}
-	
-	public Board makeValidDoku(Board b){
-		b.placeValue(5, 0, 0);   //Should be a 5 here
+
+	public Board makeValidDoku(Board b) {
+		b.placeValue(5, 0, 0); // Should be a 5 here
 		b.placeValue(3, 0, 1);
 		b.placeValue(7, 0, 4);
 		b.placeValue(6, 1, 0);
--- DeskTopSudoku/src/SudokuSolver.java
+++ DeskTopSudoku/src/SudokuSolver.java
@@ -3,28 +3,26 @@ public class SudokuSolver {
 
 	Board starty;
 	SudokuChecker sc;
-	
-	SudokuSolver(){
-		 sc = new SudokuChecker();
 
+	SudokuSolver() {
+		sc = new SudokuChecker();
 	}
-	
-	public boolean solve(Board b){
-		for(int row = 0; row<9; row++){
-			for(int col = 0; col<9; col++){
-				if(!b.isStartingVal(row, col) && b.getValAtLocation(row, col)==Nums.empty){
-					for(int k = 1; k<10; k++){
+
+	public boolean solve(Board b) {
+		for (int row = 0; row < 9; row++) {
+			for (int col = 0; col < 9; col++) {
+				if (!b.isStartingVal(row, col) && b.getValAtLocation(row, col) == Nums.empty) {
+					for (int k = 1; k < 10; k++) {
 						b.placeValue(k, row, col);
-						if(sc.isValidSudoku(b) && solve(b)){
+						if (sc.isValidSudoku(b) && solve(b)) {
 							return true;
 						}
 						b.removeValue(row, col);
 					}
-				return false;
+					return false;
 				}
 			}
 		}
-	    return true;
+		return true;
 	}
 }
-	
